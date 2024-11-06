6d073433c2709156dbae1058f69dd40f83c31240
Da-Wei Lee <daviddwlee84@gmail.com>
2024-11-06 11:00:00 +0800

    Add user login and Python default formatter

--- app.py
+++ app.py
@@ -3,10 +3,26 @@ from chainlit.input_widget import TextInput
 import os
 
 
+# https://docs.chainlit.io/authentication/password
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if password == "admin":
+        return cl.User(
+            identifier=username,
+            display_name=username,
+            metadata={"role": "admin", "provider": "credentials"},
+        )
+    else:
+        return None
+
+
 @cl.on_chat_start
 async def start():
     settings = await cl.ChatSettings(
         [
+            # https://docs.chainlit.io/api-reference/input-widgets/textinput
             TextInput(
                 id="api_key", label="API Key", initial=os.getenv("OPENAI_API_KEY")
             ),
@@ -30,4 +46,6 @@ async def setup_agent(settings: cl.ChatSettings):
 async def handle_message(message: cl.Message):
     user_api_key = cl.user_session.get("api_key")
     # Use the user_api_key for your API calls
-    await cl.Message(f"Your API key is: {user_api_key}").send()
+    await cl.Message(
+        f"Hi {cl.user_session.get('user').display_name}, Your API key is: {user_api_key}"
+    ).send()
