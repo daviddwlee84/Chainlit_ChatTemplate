fa43c13cd0779536218180f745aa53fb3086f114
Da-Wei Lee <daviddwlee84@gmail.com>
2024-11-06 15:12:45 +0800

    Enable chat history for unique sessions

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import chainlit as cl
 from chainlit.input_widget import TextInput, Switch
 import os
+import uuid
 from langchain_community.chat_models import ChatOpenAI
 from langchain_community.chat_models.fake import FakeListChatModel
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
+from langchain_core.chat_history import (
+    BaseChatMessageHistory,
+    InMemoryChatMessageHistory,
+)
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+
+def get_by_session_id(session_id: str) -> BaseChatMessageHistory:
+    store = cl.user_session.get("store", {})
+    if session_id not in store:
+        print("Initialize Chat Message History")
+        store[session_id] = InMemoryChatMessageHistory()
+        cl.user_session.set("store", store)
+    return store[session_id]
 
 
 # https://docs.chainlit.io/integrations/langchain
@@ -16,6 +31,14 @@ def setup_runnable(api_key: str, streaming: bool = True, use_fake: bool = False)
     https://python.langchain.com/docs/concepts/runnables/
     https://python.langchain.com/api_reference/core/runnables/langchain_core.runnables.base.Runnable.html
     https://python.langchain.com/v0.1/docs/expression_language/interface/
+    https://python.langchain.com/v0.1/docs/expression_language/how_to/message_history/
+
+    Chat History
+    https://python.langchain.com/docs/versions/migrating_memory/conversation_buffer_memory/#lcel-runnablewithmessagehistory
+    https://python.langchain.com/docs/versions/migrating_chains/conversation_chain/
+    https://python.langchain.com/v0.2/docs/tutorials/chatbot/#message-history
+    TODO: As of the v0.3 release of LangChain, we recommend that LangChain users take advantage of LangGraph persistence to incorporate memory into new LangChain applications.
+    https://python.langchain.com/docs/versions/migrating_memory/conversation_buffer_memory/#langgraph
     """
     if not use_fake:
         model = ChatOpenAI(api_key=api_key, streaming=streaming)
@@ -27,18 +50,29 @@ def setup_runnable(api_key: str, streaming: bool = True, use_fake: bool = False)
                 "Feel free to ask me anything.",
             ]
         )
+
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
                 "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
             ),
+            MessagesPlaceholder(variable_name="history"),
             ("human", "{question}"),
         ]
     )
 
-    runnable = prompt | model | StrOutputParser()
-    cl.user_session.set("runnable", runnable)
+    runnable = prompt | model
+    runnable_with_history = (
+        RunnableWithMessageHistory(
+            runnable=runnable,
+            get_session_history=get_by_session_id,
+            input_messages_key="question",
+            history_messages_key="history",
+        )
+        | StrOutputParser()
+    )
+    cl.user_session.set("runnable", runnable_with_history)
 
 
 # https://docs.chainlit.io/authentication/password
@@ -58,6 +92,11 @@ def auth_callback(username: str, password: str):
 
 @cl.on_chat_start
 async def start():
+    if not cl.user_session.get("store"):
+        cl.user_session.set("store", {})
+    cl.user_session.set("session_id", str(uuid.uuid4()))  # Unique session ID
+    print("Current session_id is", cl.user_session.get("session_id"))
+
     settings = await cl.ChatSettings(
         [
             # https://docs.chainlit.io/api-reference/input-widgets/textinput
@@ -88,19 +127,34 @@ async def setup_agent(settings: cl.ChatSettings):
 async def handle_message(message: cl.Message):
     runnable = cl.user_session.get("runnable")  # type: Runnable
 
+    print("Current session_id is", cl.user_session.get("session_id"))
+    history = get_by_session_id(cl.user_session.get("session_id"))
+    # print("History message:", history.messages)
+    print("History message length:", len(history.messages))
+
     if cl.user_session.get("chat_settings").get("streaming"):
         res = cl.Message(content="")
 
         async for chunk in runnable.astream(
             {"question": message.content},
-            config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+            config=RunnableConfig(
+                configurable={"session_id": cl.user_session.get("session_id")},
+                callbacks=[
+                    cl.LangchainCallbackHandler()
+                ],  # This is used to show intermediate message in Chainlit
+            ),
         ):
             await res.stream_token(chunk)
     else:
         res = cl.Message(
             content=runnable.invoke(
                 {"question": message.content},
-                config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+                config=RunnableConfig(
+                    configurable={"session_id": cl.user_session.get("session_id")},
+                    callbacks=[
+                        cl.LangchainCallbackHandler()
+                    ],  # This is used to show intermediate message in Chainlit
+                ),
             )
         )
 
